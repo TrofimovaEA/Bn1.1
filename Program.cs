06e293d2076d923eddc0d68414b74005085d5539
TrofimovaEA <trofimovahale@mai.ru>
2015-07-06 08:47:45 +0500

    Comment

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bn1._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите длину ");
+            int a = int.Parse(Console.ReadLine()); // длина
+            Console.Write("Введите ширину ");
+            int b = int.Parse(Console.ReadLine()); // ширина
+            Console.Write("Введите высоту ");
+            int c = int.Parse(Console.ReadLine()); // высота
+            double S;
+            S = 2 * (a * b + b * c + a * c); //формула площади
+            Console.WriteLine("Площадь поверхности параллелепипеда {0}", S);
+            Console.ReadKey();
+        }
+    }
+}
